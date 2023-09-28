2338efb59fe9415c965a4fe482025104d81fd18c
Nathan <lebihannathan@gmail.com>
2023-09-28 21:56:37 +0200

    remove useless files

--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -3,18 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PokemonController } from './pokemonSet.controller';
 import { PokemonService } from './services/pokemon.service';
 import { PokemonTemplateSet } from './entities/pokemonTemplateSet.entity';
-import { DataUtilsService } from './services/dataUtils.service';
-import { DataUtilsController } from './dataUtils.controller';
 import { KeyWord } from './entities/keyWord.entity';
 import { Move } from './entities/move.entity';
 import { KeyWordService } from './services/keyWord.service';
 import { MoveService } from './services/move.service';
-import { DataFromSdService } from './services/dataFromShowdown.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([PokemonTemplateSet, KeyWord, Move])],
-  controllers: [PokemonController, DataUtilsController],
-  providers: [PokemonService, DataUtilsService, DataFromSdService, KeyWordService, MoveService],
+  controllers: [PokemonController],
+  providers: [PokemonService, KeyWordService, MoveService],
   exports: [PokemonService]
 })
 export class PokemonModule { }
